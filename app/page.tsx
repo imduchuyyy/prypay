077fa838d277e6c33a16239a9543186f6f7be119
imduchuyyy <duchuy.124dk@gmail.com>
2024-11-11 17:48:38 +0700

    feat(zk): fullprove

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
-"use client"
-import useSnark from "@/stores/snarkjs";
+"use client";
 import Image from "next/image";
+import { plonk } from "snarkjs";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { snark } = useSnark();
-  console.log(snark);
+  useEffect(() => {
+    const createProof = async () => {
+      const { proof, publicSignals } = await plonk.fullProve(
+        {
+          inPublicAmount: "0",
+          outPublicAmount: "0",
+          inputAmounts: [
+            "100000000000000000000000",
+            "200000000000000000000000",
+          ],
+          inputSecrets: ["1", "2"],
+          outputAmounts: ["300000000000000000000000", "0"],
+          outputSecrets: ["3", "1"],
+        },
+        "/transfer2.wasm",
+        "/circuit_final.zkey",
+      );
+
+      console.log(proof, publicSignals);
+    };
+
+    createProof();
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,20 +1,7 @@
 "use client";
-import Script from "next/script";
-import { useEffect, useState } from "react";
-import useSnark from "@stores/snarkjs";
-
 const Providers = () => {
-  const [mounted, setMounted] = useState(false);
-  const { setSnark } = useSnark();
-  useEffect(() => {
-    if (mounted) {
-      setSnark(snarkjs);
-    }
-  }, [mounted, setSnark]);
-
   return (
     <>
-      <Script src="/snarkjs.min.js" onLoad={() => setMounted(true)} />
     </>
   );
 };
